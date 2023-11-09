dae33f96e3e451a2bb52117a40500315d7b3bfb7
maria luiza <02609526059@senacrs.edu.br>
2023-11-08 22:54:51 -0300

    a cabeca do palhaco...

--- frontend/src/Page/Profile/Profile.js
+++ frontend/src/Page/Profile/Profile.js
@@ -4,38 +4,45 @@ import MenuFeed from "../../components/MenuFeed/MenuFeed"
 import CardFeed from "../../components/CardFeed/CardFeed"
 import SignOut from "../../components/Signout/SignOut"
 import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+
 
 function Profile() {
-    // const [nome, setNome] = useState();
+    const [nome, setNome] = useState();
+    const [userData, setUserdata] = useState({});
+    const id = localStorage.getItem("id");
 
-    // const fetchData = async () => {
-    //     const id = localStorage.getItem('id');
-    //     //alert(id);
-    //     if (id) {
-    //       try {
-    //         const response = await api.get('/user/' + id);
-    //         console.log(response);
+
+    const fetchData = async () => {
+        const id = localStorage.getItem('id');
+        //alert(id);
+        if (id) {
+          try {
+            const response = await api.get('/user/' + id);
+            console.log(response);
             
-    //         setUserdata(response.data.data[0]);
-    //         console.log(userData);
-    //       } catch (error) {
-    //         console.error('Error fetching user data:', error);
-    //       }
+            setUserdata(response.data.data[0]);
+            console.log('aaaaaaaaaaaaaaa', userData);
+          } catch (error) {
+            console.error('Error fetching user data:', error);
+          }
     
-    //     }
-    //   }
-    
-    
+        }
+      }
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+
     return (
         <>
-
             <ProfileSection>
                 <SignOut/>
                 <ImgDivProfile>
                     <ImgProfile src={ProfileImg} />
                 </ImgDivProfile>
-                <TittleProfile>
-                    @Maria Luiza Ferrari
+                <TittleProfile                    
+                nome={userData.nome}>@
                 </TittleProfile>
                 <TextProfile>
                     Oii, sou a Malu amo dançar e cantar!!
